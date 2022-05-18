0a278d95661e7c7752cdf8c0be53bdd39d89f0ac
Daiane Oltramari <daianeoltramari@gmail.com>
2022-05-18 17:35:09 -0300

    menu alterado

--- src/pages/menu/menu.jsx
+++ src/pages/menu/menu.jsx
@@ -1,40 +1,84 @@
-import React, { useState } from 'react';
-import  useProducts  from './useProducts'; 
-import ProductCards from '../../components/productInfos';
-
+//import React, { useState } from "react";
+import useProducts from "./useProducts";
+import ProductCards from "../../components/productInfos";
 
 const Menu = () => {
-  const [items, setItems] = useState([]);
-  const { 
+  const {
     handleButtonTypeClick,
-    productsFiltered 
+    productsFiltered,
+    productsType,
+    handleSelectFlavor,
+    handleSelectComplement,
+    handleAddItem,
+    items,
   } = useProducts();
-  const handleAddItem = (product) => {
-    console.log(product)
-    const productIndex = items.findIndex((item) => {
-      return item.id === product.id
-    })
-    if(productIndex === -1) {
-      setItems([...items, {...product, qtd: 1}])
-    } else {
-      items[productIndex].qtd += 1
-      setItems([...items])
-    }
-  }
+
   return (
-    <main className='main'>
+    <main className="main">
       <p> MENU </p>
-        <div className='menu-types'>
-          <button className='menu-button' onClick={handleButtonTypeClick} value={'breakfast'}>Café da manhã</button>
-          <button className='menu-button' onClick={handleButtonTypeClick} value={'allday'}>O dia todo</button>
-        </div>
-        <div>
-          <ul>
-            {items.map((item, index) => <li key={index}>{item.name} {item.qtd} </li>)}
-          </ul>
-          {productsFiltered().map((element, index) => {
-            return (
-              <ProductCards
+      <div className="menu-types">
+        <button
+          className="menu-button"
+          onClick={handleButtonTypeClick}
+          value={"breakfast"}
+        >
+          Café da manhã
+        </button>
+        <button
+          className="menu-button"
+          onClick={handleButtonTypeClick}
+          value={"hamburguer"}
+        >
+          Hambúrgueres
+        </button>
+        <button
+          className="menu-button"
+          onClick={handleButtonTypeClick}
+          value={"side"}
+        >
+          Acompanhamentos
+        </button>
+        <button
+          className="menu-button"
+          onClick={handleButtonTypeClick}
+          value={"drinks"}
+        >
+          Bebidas
+        </button>
+      </div>
+      {productsType === "hamburguer" ? (
+        <section>
+          <select onChange={handleSelectFlavor}>
+            <option value="escolher o sabor" selected disabled>
+              Escolher o sabor
+            </option>
+            <option value="carne">Carne</option>
+            <option value="frango">Frango</option>
+            <option value="vegetariano">Vegetariano</option>
+          </select>
+          <select onChange={handleSelectComplement}>
+            <option value="escolher complemento" disabled selected>
+              Escolher complemento
+            </option>
+            <option value="null">Sem complemento</option>
+            <option value="queijo">Queijo</option>
+            <option value="ovo">Ovo</option>
+          </select>
+        </section>
+      ) : (
+        ""
+      )}
+      <div>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              {item.name} {item.qtd}{" "}
+            </li>
+          ))}
+        </ul>
+        {productsFiltered().map((element, index) => {
+          return (
+            <ProductCards
               key={index}
               image={element.image}
               name={element.name}
@@ -42,10 +86,10 @@ const Menu = () => {
               complement={element.complement}
               price={element.price}
               onClick={() => handleAddItem(element)}
-              />
-            )
-          })}
-        </div>
+            />
+          );
+        })}
+      </div>
     </main>
   );
 };
