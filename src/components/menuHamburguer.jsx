af580ad24c77dd13e78acefada27c5847776dac9
Daiane Oltramari <daianeoltramari@gmail.com>
2022-05-20 19:16:43 -0300

    menu hamburguer

--- src/components/menuHamburguer.jsx
+++ src/components/menuHamburguer.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styles from './components.module.css';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./components.module.css";
 
 const MenuHamburguer = () => {
   const navigate = useNavigate();
   const [active, setMode] = useState(false);
   const ToggleMode = () => {
     setMode(!active);
-  }
+  };
   return (
     <>
-      <section className={active ? 'icon iconActive' : 'icon'} onClick={ToggleMode}>
-        <div className='hamburguer hamburguerIcon'></div>
+      <section
+        className={active ? "icon iconActive" : "icon"}
+        onClick={ToggleMode}
+      >
+        <div className="hamburguer hamburguerIcon"></div>
       </section>
-      <section className={active ? 'menu menuOpen' : 'menu menuClose'}>
+      <section className={active ? "menu menuOpen" : "menu menuClose"}>
         <div className={styles.hamburguerMenuList}>
           <ul className={styles.hamburguerMenuListItems}>
             <li>
@@ -23,14 +26,24 @@ const MenuHamburguer = () => {
               <button>COZINHA</button>
             </li>
             <li>
-              <button>PEDIDOS PRONTOS</button>
+              <button
+                onClick={() => {
+                  navigate("/order");
+                }}
+              >
+                PEDIDOS PRONTOS
+              </button>
             </li>
             <li className={styles.logoutButton}>
-              <button onClick={() => {
-                localStorage.removeItem('token');
-                localStorage.removeItem('role');
-                navigate('/');
-              }}>SAIR</button>
+              <button
+                onClick={() => {
+                  localStorage.removeItem("token");
+                  localStorage.removeItem("role");
+                  navigate("/");
+                }}
+              >
+                SAIR
+              </button>
             </li>
           </ul>
         </div>
