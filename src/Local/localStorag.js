e93aa678bb3c310fbaea2890891c677b0c02bc08
Daiane Oltramari <daianeoltramari@gmail.com>
2022-05-23 19:41:50 -0300

    alterações

--- src/Local/localStorag.js
+++
@@ -1,9 +0,0 @@
-export const URL = 'http://lab-api-bq.herokuapp.com';
-
-export const TokenAndRole = (token, role) => {
-  localStorage.setItem('token', token);
-  localStorage.setItem('role', role);
-}
-
-export const getToken = () => localStorage.getItem('token');
-export const getRole = () => localStorage.getItem('role');
